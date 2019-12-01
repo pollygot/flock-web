027193ad2e49f51d024827d035e48910581f9ba8
Paul Copplestone <pcopplestone@gmail.com>
2019-12-01 15:00:00 +0800

    Init

---
+++ docusaurus.config.js
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  title: "Flock",
+  tagline:
+    "All of your external systems in one easy to use RESTful API.",
+  url: "https://flock.rest",
+  baseUrl: "/",
+  favicon: "favicon.ico",
+  organizationName: "Flock", // Usually your GitHub org/user name.
+  projectName: "flock", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      // title: 'My Site',
+      logo: {
+        alt: "Flock",
+        src: "img/logo.svg"
+      },
+      links: [
+        { to: "docs/doc1", label: "Docs", position: "left" },
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/flock/flock",
+          label: "GitHub",
+          position: "right"
+        }
+      ]
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Docs",
+              to: "docs/doc1"
+            }
+          ]
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus"
+            }
+          ]
+        },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "Blog",
+              to: "blog"
+            }
+          ]
+        }
+      ],
+      logo: {
+        alt: "Flock",
+        src: "/img/logo-white.svg",
+        // href: "https://opensource.facebook.com/"
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} Flock.`
+    }
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js")
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
+};
